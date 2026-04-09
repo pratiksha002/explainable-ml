6223ab04927e3e32a5a3618d99d2b40a2a51f449
pratiksha002 <020606pratiksha@gmail.com>
2026-04-09 21:22:01 +0530

    Add changes in shap

--- app/explainers/shap_explainer.py
+++ app/explainers/shap_explainer.py
@@ -2,12 +2,36 @@ import shap
 import pickle
 import pandas as pd
 
+
 with open("app/models/model.pkl", "rb") as f:
     model = pickle.load(f)
 
 explainer = shap.Explainer(model)
 
+feature_names = [
+    "MedInc", "HouseAge", "AveRooms", "AveBedrms",
+    "Population", "AveOccup", "Latitude", "Longitude"
+]
+
 def explain_shap(input_df):
     shap_values = explainer.shap_values(input_df)
 
-    return shap_values[0].tolist()
+    if hasattr(shap_values, "values"):
+        values = shap_values.values[0]
+    else:
+        values = shap_values[0]
+
+    
+    explanation = {
+        feature: float(value)
+        for feature, value in zip(feature_names, values)
+    }
+
+    explanation = dict(
+        sorted(
+            explanation.items(),
+            key=lambda x: abs(x[1]),
+            reverse=True
+        )
+    )
+    return explanation
