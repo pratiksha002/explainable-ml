0ce8288dda1acbf8935bef8c85419bcd2c4ae294
pratiksha002 <020606pratiksha@gmail.com>
2026-04-16 15:50:46 +0530

    Added lime explainer

---
+++ app/explainers/lime_explainer.py
@@ -0,0 +1,23 @@
+from lime.lime_tabular import LimeTabularExplainer
+import numpy as np
+import pandas as pd
+import pickle
+
+with open("app/models/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+x_train = pd.read_csv("data/x_train.csv")
+
+explainer = LimeTabularExplainer(
+    training_data=np.array(x_train),
+    feature_names=x_train.columns.tolist(),
+    mode="regression"
+)
+
+def explain_lime(input_df):
+    exp = explainer.explain_instance(
+        input_df.iloc[0],
+        model.predict
+    )
+
+    return dict(exp.as_list())
